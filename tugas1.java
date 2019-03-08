7717d402bd97ff37c53906054cc9574c8bba8e58
Ilham Pratama <48341192+IlhamBintangPratama@users.noreply.github.com>
2019-03-08 10:19:28 +0700

    Create tugas1.java

---
+++ tugas1.java
@@ -0,0 +1,8 @@
+public class B-Ilham {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Ilham Bintang Pratama");
+        System.out.println("NIM     : 17090022");
+        System.out.println("Kelas   : 4B");
+    }
+    
+}
